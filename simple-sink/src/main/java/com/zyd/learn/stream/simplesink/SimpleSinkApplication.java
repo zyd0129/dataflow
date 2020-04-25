f0f82ce60552cbc7b3afb766c49b1de717ecf4ca
zyd0129 <zyd_0129@qq.com>
2020-04-25 09:09:51 +0800

    init project

---
+++ simple-sink/src/main/java/com/zyd/learn/stream/simplesink/SimpleSinkApplication.java
@@ -0,0 +1,13 @@
+package com.zyd.learn.stream.simplesink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSinkApplication.class, args);
+    }
+
+}
---
+++ simple-sink/src/test/java/com/zyd/learn/stream/simplesink/SimpleSinkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zyd.learn.stream.simplesink;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleSinkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
